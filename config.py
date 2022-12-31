b55f2eb8bdd9bc1d906d0c50856ff4372291649a
jackeyli <40691963+jackeyli81@users.noreply.github.com>
2022-12-31 12:19:31 +0000

    add config(uncompleted)

---
+++ config.py
@@ -0,0 +1,12 @@
+# encoding=utf8
+import os
+import preprocess
+
+script_status_info = 'uncompleted'
+raise Exception(script_status_info)
+
+root_path = os.path.dirname(
+    os.path.abspath(__file__)
+)
+
+tmp_vars = preprocess.Open()
